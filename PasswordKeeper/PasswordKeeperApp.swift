fd4f396cea4e9949a89dff031ce2e855e15d11ec
Jegor Jegorov <dev@jegorov.eu>
2023-04-11 19:44:23 +0300

    Initial Commit

---
+++ PasswordKeeper/PasswordKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordKeeperApp.swift
+//  PasswordKeeper
+//
+//  Created by Jegor on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PasswordKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
